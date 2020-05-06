0dd9171b7ff4b97b96f0f93e41313340a5ffc8ec
MTK3852 <mtk835123@gmail.com>
2020-05-06 17:43:52 +0300

    Third lab task two

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace LAB3._1OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fc = new StreamReader("e:\\C#\\input.txt");
+            string s = fc.ReadLine().Trim();
+            Console.WriteLine("Сontents:");
+            Console.WriteLine(s);
+
+            Console.WriteLine("Enter word:");
+            string word = Console.ReadLine();
+            string[] array = s.Split(".");
+            foreach (string it in array)
+            {
+                if (it.Contains(word))
+                {
+                    Console.WriteLine(it);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
